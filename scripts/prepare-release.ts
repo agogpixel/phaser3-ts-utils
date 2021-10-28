d21683e03fe7c022de898a365268a30fabd2a240
kidthales <kidthales@agogpixel.com>
2021-10-28 16:33:18 -0700

    Attempt automated release refinement.

--- scripts/prepare-release.ts
+++ scripts/prepare-release.ts
@@ -6,14 +6,17 @@ const packageJson = require('../dist/package.json');
 
 const isDevRelease = process.argv.length > 2 && process.argv[2] === 'dev';
 
+const timestamp = Math.floor(Date.now() / 1000);
+
 const tarballPrefix = 'phaser3-ts-utils';
 
-const tarballPostfix = `${packageJson.version}` + (isDevRelease ? `-dev.${Math.floor(Date.now() / 1000)}` : '');
+const tarballPostfix = `${packageJson.version}` + (isDevRelease ? `-dev.${timestamp}` : '');
+
+const srcTarball = `agogpixel-${tarballPrefix}-${tarballPostfix}.tgz`;
+const dstTarball = isDevRelease ? `${tarballPrefix}-dev.tgz` : `${tarballPrefix}-${tarballPostfix}.tgz`;
 
 function pack() {
-  const mvCmd =
-    `mv agogpixel-${tarballPrefix}-${tarballPostfix}.tgz ` +
-    (isDevRelease ? `../${tarballPrefix}-latest.tgz` : `../${tarballPrefix}-${tarballPostfix}.tgz`);
+  const mvCmd = `mv ${srcTarball} ../${dstTarball}`;
 
   exec(`cd dist && npm pack && ${mvCmd} && cd ..`, (err, stdout, stderr) => {
     if (err) {
@@ -21,15 +24,33 @@ function pack() {
       throw err;
     }
 
-    console.log(stdout);
-    console.log(stderr);
+    writeFile(
+      'release-log.txt',
+      `[${timestamp}]\nPrepare Release: ${dstTarball}\n\nstdout:\n\n${stdout}\n\nstderr:\n\n${stderr}\n\n`,
+      { flag: 'a', encoding: 'utf-8' },
+      (err) => {
+        if (err) {
+          console.error(err.message);
+          throw err;
+        }
+
+        writeFile('release-tarball-name.txt', dstTarball, { flag: 'w', encoding: 'utf-8' }, (err) => {
+          if (err) {
+            console.error(err.message);
+            throw err;
+          }
+
+          console.log(dstTarball);
+        });
+      }
+    );
   });
 }
 
 if (isDevRelease) {
   packageJson.version = tarballPostfix;
 
-  writeFile('dist/package.json', JSON.stringify(packageJson, undefined, 2), 'utf-8', (err) => {
+  writeFile('dist/package.json', JSON.stringify(packageJson, undefined, 2), { flag: 'w', encoding: 'utf-8' }, (err) => {
     if (err) {
       console.error(err.message);
       throw err;
