fc4b91e8e114f32d60612b177e11789776762560
kidthales <kidthales@agogpixel.com>
2021-10-28 20:31:38 -0700

    Update release log output.

--- scripts/publish-release.ts
+++ scripts/publish-release.ts
@@ -8,7 +8,14 @@ if (!tarball) {
   throw new Error();
 }
 
-const tag = process.argv.length > 3 ? process.argv[3] : 'latest';
+const registry = process.argv.length > 3 ? process.argv[3] : undefined;
+
+if (!registry) {
+  console.error('Registry name not provided');
+  throw new Error();
+}
+
+const tag = process.argv.length > 4 ? process.argv[4] : 'latest';
 
 exec(`npm publish ${tarball} --tag ${tag} --access public`, (err, stdout, stderr) => {
   if (err) {
@@ -18,7 +25,7 @@ exec(`npm publish ${tarball} --tag ${tag} --access public`, (err, stdout, stderr
 
   writeFile(
     'release-log.txt',
-    `Publish Release Package: ${tarball}\n\nstdout:\n\n${stdout}\n\nstderr:\n\n${stderr}`,
+    `Publish Release Package: ${registry}\n\nstdout:\n\n${stdout}\n\nstderr:\n\n${stderr}`,
     { flag: 'a', encoding: 'utf-8' },
     (err) => {
       if (err) {
